250266abe0f67969aa4929545e04127f1a662dd8
Rom Rakharb <rom.rakharb@mailfence.com>
2024-10-02 17:54:15 +0700

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Simple rust dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+
+    devShells.x86_64-linux.default = pkgs.mkShell {
+
+      packages = with pkgs; [
+        rustc
+        cargo
+        clippy
+        rustfmt
+        rust-analyzer
+      ];
+
+    };
+
+  };
+}
